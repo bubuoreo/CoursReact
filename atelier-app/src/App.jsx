08e50a7472dacc7b0ee266ae33b7e3a0a115acee
QuentinMELERO <Quentin.melero@hotmail.fr>
2023-12-08 08:40:15 +0100

    Création pages

--- atelier-app/src/App.jsx
+++ atelier-app/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './HomePage';
+import BuyPage from './BuyPage';
+import SellPage from './SellPage';
+import RegistrationPage from './RegistrationPage';
 
-export const App = (props) => {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-      <div>
-        <h1>Bonjour</h1>
-      </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegistrationPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/buy" component={BuyPage} />
+        <Route path="/sell" component={SellPage} />
+        <Route path="/" exact component={LoginPage} />
+        {/* Redirect to login page if no match found */}
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App
+export default App;
