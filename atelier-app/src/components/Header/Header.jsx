459c94f73f3e23b1a9303f8005ee53a8d1e73ce4
adriencorsetti <adrien.corsetti@cpe.fr>
2023-12-13 17:54:10 +0100

    css

--- atelier-app/src/components/Header/Header.jsx
+++ atelier-app/src/components/Header/Header.jsx
@@ -7,7 +7,8 @@ export const Header = () => {
   let user = useSelector(state => state.userReducer.user);
   console.log(user)
   return (
-    <header className="w-full flex justify-between items-center px-4 py-2 bg-gray-200 shadow-md">
+
+    <header>
       <h1 className="text-xl font-bold text-center">My Application</h1>
       <User name={user.login} email={user.email} balance={user.account} />
     </header>
--- atelier-app/src/components/User/User.jsx
+++ atelier-app/src/components/User/User.jsx
@@ -4,20 +4,19 @@ import { FaUserCircle } from 'react-icons/fa'; // Make sure to install react-ico
 const User = ({ name, email, balance }) => {
 
   return (
-    <div className="container mx-auto">
-    <div className="grid grid-cols-3 gap-4 border p-4">
-      <div className="flex items-center">
-        <FaUserCircle className="text-xl" aria-hidden="true" />
-      </div>
-      <div className="flex flex-col">
-        <span className="font-semibold text-sm">{name}</span>
-        <span className="text-xs text-gray-600">{email}</span>
-      </div>
-      <div className="flex items-end justify-end">
-        <span className="text-sm font-bold">${balance}</span>
-      </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 col-lg-11" >
+        
+          <span className="text-sm font-bold ">Balance: ${balance}</span>  
+        </div>
+        <div className="col-md-6 col-lg-1" >
+        <FaUserCircle style={{ fontSize: '2.5rem' }} aria-hidden="true" />
+          <span className="font-semibold text-sm">  {name}</span>
+        </div>
+      </div> 
     </div>
-  </div>
+
 );
 };
 
