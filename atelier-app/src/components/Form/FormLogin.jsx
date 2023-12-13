e141e0bfb93a73fed837ea171a17bea03b5982b0
adriencorsetti <adrien.corsetti@cpe.fr>
2023-12-13 13:41:56 +0100

    Form linked to backend

--- atelier-app/src/components/Form/FormLogin.jsx
+++ atelier-app/src/components/Form/FormLogin.jsx
@@ -2,36 +2,55 @@ import React, { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+
+
 const FormLogin = ({ onConnect, onCancel }) => {
-  const [surname, setSurname] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onConnect(surname, password);
-  };
-
   const navigate = useNavigate();
 
-  const handleSignIn = () => {
-    // Directly navigate to the home page without credentials check
-    navigate('/home');}
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:8083/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        username: username,
+        password: password,
+       }),
+    });
+    if (response.status === 403){
+      console.log("invalid credentials");
+    }
+    else{
+      const data  = await response.json();
+      console.log(data);
+      navigate('/home');
+
+    }
+    
+
+    }
+
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-full max-w-xs">
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="surname">
-              Surname
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="Username">
+              Username
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="surname"
+              id="username"
               type="text"
-              placeholder="Surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mb-6">
@@ -48,7 +67,7 @@ const FormLogin = ({ onConnect, onCancel }) => {
             />
           </div>
           <div className="flex items-center justify-between">
-            <button onClick={handleSignIn} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            <button onClick={handleSubmit} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
               Connect
             </button>
             <button className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" onClick={onCancel}>
--- atelier-app/src/components/Form/FormSignUp.jsx
+++ atelier-app/src/components/Form/FormSignUp.jsx
@@ -1,69 +1,108 @@
 import React, { useState } from 'react';
 
 const UserForm = ({ onSubmit, onCancel }) => {
-  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [surname, setSurname] = useState('');
+  const [money, setMoney] = useState(0);
   const [password, setPassword] = useState('');
-  const [rePassword, setRePassword] = useState('');
+  const [lastname, setLastname] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    if (password !== rePassword) {
-      alert("Passwords do not match.");
-      return;
+
+    const response = await fetch('http://localhost:8083/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        id:0,
+        login:username,
+        pwd: password,
+        email:email,
+        surName:surname,
+        account:money,
+        lastName:lastname,
+        cardList: [0],
+       }),
+    });
+    const data  = await response.json();
+    console.log(data);
+
     }
-    // Pass the form data up to the parent component or handle it here
-    onSubmit({ name, surname, password });
-  };
+    
+  
 
   return (
     <form onSubmit={handleSubmit} className="user-form">
-      <div className="form-field">
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="surname">Surname</label>
-        <input
-          id="surname"
-          type="text"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-          required
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="rePassword">Re-Password</label>
-        <input
-          id="rePassword"
-          type="password"
-          value={rePassword}
-          onChange={(e) => setRePassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="form-actions">
-        <button type="button" onClick={onCancel}>Cancel</button>
-        <button type="submit">OK</button>
-      </div>
-    </form>
-  );
+    <div className="form-field">
+    <label htmlFor="username">Username</label>
+    <input
+      id="username"
+      type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-field">
+    <label htmlFor="email">Email</label>
+    <input
+      id="email"
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-field">
+    <label htmlFor="surname">First Name</label>
+    <input
+      id="surname"
+      type="text"
+      value={surname}
+      onChange={(e) => setSurname(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-field">
+    <label htmlFor="lastname">Last Name</label>
+    <input
+      id="lastname"
+      type="text"
+      value={lastname}
+      onChange={(e) => setLastname(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-field">
+    <label htmlFor="money">Money</label>
+    <input
+      id="money"
+      type="number"
+      value={money}
+      onChange={(e) => setMoney(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-field">
+    <label htmlFor="password">Password</label>
+    <input
+      id="password"
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+    />
+    </div>
+    <div className="form-actions">
+    <button type="button" onClick={onCancel}>Cancel</button>
+    <button type="submit">OK</button>
+    </div>
+
+      </form>
+    );
 };
 
 export default UserForm;
