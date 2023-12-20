986c2d795fdfb4041ae1f8c2bed6f0339b947c14
adriencorsetti <adrien.corsetti@cpe.fr>
2023-12-20 09:53:38 +0100

    table card sell

--- atelier-app/src/components/Card/containers/CardItem.jsx
+++ atelier-app/src/components/Card/containers/CardItem.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const CardItem = ({ card, onBuy }) => (
-  <div className="card">
-    <h3>{card.name}</h3>
-    <p>{card.description}</p>
-    <p>{card.price}</p>
-    <button onClick={() => onBuy(card.id)}>Buy</button>
-  </div>
+
+  <tr>
+    <td>
+      <img  class="ui avatar image" src={card.smallImgUrl}/> <span>{card.name}</span>
+    </td>
+    <td>{card.description}</td>
+    <td>{card.family}</td>
+    <td>{card.hp}</td>
+    <td>{card.energy}</td>
+    <td>{card.defense}</td>
+    <td>{card.attack}</td>
+    <td>{card.price}$</td>
+    <td>
+        <div class="ui vertical animated button" tabindex="0">
+            <div class="hidden content" onClick={() => onBuy(card.id)}>Buy</div>
+            <div class="visible content">
+                <i class="shop icon"></i>
+            </div>
+        </div>
+    </td>
+  </tr>
 );
 
 export default CardItem;
--- atelier-app/src/components/User/containers/UserCardItem.jsx
+++ atelier-app/src/components/User/containers/UserCardItem.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 const UserCardItem = ({ card, onSell }) => (
-  <div className="card-item">
-    <h3>{card.name}</h3>
-    <p>{card.description}</p>
-    <p>{card.price}</p>
-    <img src={card.imgUrl} alt={card.name} />
-    <button onClick={() => onSell(card.id)}>Sell</button>
-  </div>
+
+  <tr>
+    <td>
+      <img  class="ui avatar image" src={card.smallImgUrl}/> <span>{card.name}</span>
+    </td>
+    <td>{card.description}</td>
+    <td>{card.family}</td>
+    <td>{card.hp}</td>
+    <td>{card.energy}</td>
+    <td>{card.defence}</td>
+    <td>{card.attack}</td>
+    <td>{card.price}$</td>
+    <td>
+        <div class="ui vertical animated button" tabindex="0">
+            <div class="hidden content" onClick={() => onSell(card.id)}>Sell</div>
+            <div class="visible content">
+                <i class="shop icon"></i>
+            </div>
+        </div>
+    </td>
+  </tr>
 );
 
 export default UserCardItem;
